d082ca9a15a0b939b7d066e5691ef64e0dd9ce2c
Ygor <githubygoraraujo@gmail.com>
2025-04-28 16:35:59 -0300

    att

---
+++ Back-end/src/main/java/com/GerenciamentoHP/DTO/CampodeErros.java
@@ -0,0 +1,4 @@
+package com.GerenciamentoHP.DTO;
+
+public record CampodeErros(String campo , String erro) {
+}
---
+++ Back-end/src/main/java/com/GerenciamentoHP/DTO/RespostaErro.java
@@ -0,0 +1,16 @@
+package com.GerenciamentoHP.DTO;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record RespostaErro(int status, String mensagem, List<CampodeErros> erros) {
+
+    public static RespostaErro respstaPadrao(String mensagem) {
+        return new RespostaErro(HttpStatus.BAD_REQUEST.value(), mensagem, List.of());
+    }
+
+    public static RespostaErro conflito(String mensagem) {
+        return new RespostaErro(HttpStatus.CONFLICT.value(), mensagem, List.of());
+    }
+}
